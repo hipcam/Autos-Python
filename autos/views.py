a23c894a91ad5c97f8da6c8c546b51cf11bab65e
Fernanda Alvarado <alessandrafernanda.alvarado@tecsup.edu.pe>
2018-07-31 00:21:54 -0500

    tests

--- autos/views.py
+++ autos/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 import pandas as pd 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 df = pd.read_csv(url, header = None)
-df.head(5)
-path="C:\Users\FERNANDA_ALEXIS\Downloads\automoviles.csv"
+print("Done")
+# create headers list
+headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num-of-doors","body-style",
+         "drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type",
+         "num-of-cylinders", "engine-size","fuel-system","bore","stroke","compression-ratio","horsepower",
+         "peak-rpm","city-mpg","highway-mpg","price"]
+headers
+df.columns = headers
+df.head(10)
+df.dropna(subset=["price"], axis=0)
+path=r"C:\Users\FERNANDA_ALEXIS\Downloads\automoviles.csv"
 df.to_csv(path)
